cfb3e5b3e71e9b64b1b682b7f3e39f51723f0678
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-05-31 10:10:01 +0200

    contenu page festival sans le carroussel

---
+++ src/components/RectanglePresentation.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="mt-14">
+    <p class=" text-2xl font-medium text-center text-white relative z-40 mt-10 font-lato">{{titre}}</p>
+    <svg
+      width="375"
+      height="88"
+      viewBox="0 0 375 88"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-full h-[87.5px] relative -mt-14 z-30"
+      preserveAspectRatio="none"
+    >
+      <rect width="375" height="87" :fill="couleur"></rect>
+      <path d="M278 44.5L316 87H337L278 44.5Z" fill="white"></path>
+      <path d="M76 45L38 87.5H17L76 45Z" fill="white"></path>
+      <path d="M37 42.5L75 0H96L37 42.5Z" fill="white"></path>
+      <path d="M337 42.5L299 0H278L337 42.5Z" fill="white"></path>
+    </svg>
+</div>
+
+</template>
+
+<script>
+
+export default {
+    props: {
+        titre: String,
+        couleur: String,
+    },
+}
+</script>
--- src/views/FestivalView.vue
+++ src/views/FestivalView.vue
@@ -1,5 +1,84 @@
 <template>
-    <p>
-        ici la page festival
-    </p>
+    <main>
+        <div>
+            <RectanglePresentation
+            titre="À propos du festival"
+            couleur="#4C1D95"/>
+        </div>
+
+
+        <section class="flex flex-row justify-center mt-14 lg:hidden">
+            <div>
+                <HautTriangle/>
+                <p class="w-[343px] pb-6 font-montserrat font-normal text-xs"> Spread Night est un festival de musique exclusivement réservé au genre Drum and Bass.
+                </p>
+                <p class="w-[343px] pb-6 font-montserrat font-normal text-xs">
+                    Il se déroulera durant 7 jours pendant les vacances de la Toussaint 2023. C’est un moment parfait pour ne pas avoir des températures extrêmes.
+                </p>
+                <p class="w-[343px] font-montserrat font-normal text-xs"> Avec Spread Night vous serez embarqué dans un autre monde puisque nous possédons des décors éblouissants qui vous permettront pendant un moment de vous échapper de la réalité et de profiter à 200% du concert. 
+                </p>
+                <div class="flex flex-row justify-center">
+                    <BasTriangle/>
+                </div>
+            </div>
+        </section>
+        <section class=" flex-row justify-center mt-14 hidden lg:flex">
+            <div>
+                <HautTriangle class="w-[443px]"/>
+                <p class="w-[500px] pb-6 font-montserrat font-normal text-lg"> Spread Night est un festival de musique exclusivement réservé au genre Drum and Bass. 
+                </p>
+                <p class="w-[500px] pb-6 font-montserrat font-normal text-lg">
+                    Il se déroulera durant 7 jours pendant les vacances de la Toussaint 2023. C’est un moment parfait pour ne pas avoir des températures extrêmes.
+                </p>
+                <p class="w-[500px] font-montserrat font-normal text-lg">
+                    Avec Spread Night vous serez embarqué dans un autre monde puisque nous possédons des décors éblouissants qui vous permettront pendant un moment de vous échapper de la réalité et de profiter à 200% du concert. 
+                </p>
+                <div class="flex flex-row justify-center">
+                    <BasTriangle class="w-[97px]"/>
+                </div>
+            </div>
+        </section>
+        <section class="flex flex-col justify-center">
+            <h2 class="font-lato text-lg font-semibold text-center mt-14 lg:hidden">Mais où se situe exactement le festival ?</h2>
+            <h2 class="font-lato text-2xl font-semibold justify-center mt-14 hidden lg:flex">Mais où se situe exactement le festival ?</h2>
+            <div class="flex flex-row justify-center mt-12 mx-9 lg:hidden">
+                <p class="font-montserrat font-normal text-xs w-[724px]">Rien de plus simple, notre festival est situé sur le point ci-dessous. Plus précisément, il se situe à 2km de chamonix ! C’est donc bien au pied des alpes que ce festival grandiose aura lieu.</p>
+            </div>
+            <div class="flex-row justify-center mt-12 mx-9 hidden lg:flex">
+                <p class="font-montserrat font-normal text-xl w-[724px]">Rien de plus simple, notre festival est situé sur le point ci-dessous. Plus précisément, il se situe à 2km de chamonix ! C’est donc bien au pied des alpes que ce festival grandiose aura lieu.</p>
+            </div>
+            <div class="flex flex-row justify-center mt-16 lg:hidden">
+                <img src="../../public\Images\AnyConv.com__Group 9.webp" alt="Carte de France">
+            </div>
+            <div class=" hidden flex-row justify-center mt-16 lg:flex">
+                <img class="w-[400px]" src="../../public\Images\AnyConv.com__Group 9.webp" alt="Carte de France">
+            </div>
+            <div class="justify-center flex mb-[86px]">
+                <BoutonAction
+                titre="Voir la map"
+                lien="/contact"/>
+            </div>
+        </section>
+        <section>
+            <RectanglePresentation
+            titre="Galerie photos"
+            couleur="#4C1D95"/>
+
+            <p class="font-montserrat font-normal text-xs mt-16 mb-8 ml-5 lg:hidden">Voici quelques photos du festival de quoi passer des moments magiques !</p>
+            <p class="font-montserrat font-normal text-base mt-16 mb-8 ml-5 hidden lg:flex">Voici quelques photos du festival de quoi passer des moments magiques !</p>
+        </section>
+    </main>
 </template>
+
+<script>
+import RectanglePresentation from '../components/RectanglePresentation.vue'
+import HautTriangle from '../components/Icons/HautTriangle.vue'
+import BasTriangle from '../components/Icons/BasTriangle.vue'
+import BoutonAction from '../components/BoutonAction.vue'
+
+export default {
+    name:"Accueil",
+    components:{RectanglePresentation, HautTriangle, BasTriangle, BoutonAction}
+
+}
+</script>
