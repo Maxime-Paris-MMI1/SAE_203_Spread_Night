e8798825cd085df623de7f92fe7ae2ea0e84faf6
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-05-31 12:35:19 +0200

    essaie mise en pages artistes

--- src/views/ListeArtistes.vue
+++ src/views/ListeArtistes.vue
@@ -1,5 +1,33 @@
 <template>
-    <main>
+    <main class="flex flexrow flex-wrap gap-20 mt-10 mb-20">
+        <CarteArtiste
+        titrebouton="En savoir plus"
+        chansonconnue="Animals"
+        artiste="Martin Garrix"
+        image="../../public\Images\medias_2019_07_martin-garrix_5d2319d26058b.webp"
+        />
+
+        <CarteArtiste
+        titrebouton="En savoir plus"
+        chansonconnue="Broken"
+        artiste="Netsky"
+        image="../../public\Images\Netsky_(DJ).webp"
+        />
+
+        <CarteArtiste
+        titrebouton="En savoir plus"
+        chansonconnue="Heartbeat Loud"
+        artiste="Andy C"
+        image="../../public\Images\pexels-binyamin-mellish-106399 1.png"
+        />
+
+        <CarteArtiste
+        titrebouton="En savoir plus"
+        chansonconnue="Animals"
+        artiste="Martin garrix"
+        image="../../public\Images\medias_2019_07_martin-garrix_5d2319d26058b.webp"
+        />
+
         <CarteArtiste
         titrebouton="En savoir plus"
         chansonconnue="Animals"
