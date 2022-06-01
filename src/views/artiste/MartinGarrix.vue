0a76a46142e3cdb48d15ad3ad9e440e5245a8ef6
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-06-01 12:12:27 +0200

    page artiste martin garrix fini

--- src/views/artiste/MartinGarrix.vue
+++ src/views/artiste/MartinGarrix.vue
@@ -20,8 +20,44 @@
             - Il connait un grand succès dès l’âge de 17 ans avec la sortie d' <a href="https://youtu.be/gCYcHz2k5x0"><span class="underline">Animals</span></a> qui comporte plus d’un milliard de vue et plus de 10 millions de likes ! <br/>
             - Aujourd’hui c’est un grand DJ qui est connu par beaucoup de musiques. <br/>
             </p>
-            
+
             <p class="font-lato text-3xl font-medium">Ses plus grands succcès: </p>
+            
+        </div>
+        <div class="my-16 ml-1 justify-center items-center gap-7 mt-20 mb-20 grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] sm:ml-5 ">
+            <div class="flex flex-row">
+                <img class="max-h-40" src="public/Images/AnyConv.com__image2.webp" alt="">
+                <div class="w-[150px] flex-col pl-2 bg-[#E2E8F0]">
+                    <p class="font-lato text-2xl mb-3">1</p> <br/>
+                    <p class="-mt-10 font-lato font-semibold">Animals</p> <br/>
+                    <p class=" font-montserrat text-black -mt-4 text-xs w-[145px]">
+                    - Plus d’1 milliard de vues <br/>
+                    - 4 millions de ventes de disque aux États Unis et plus de 400 000 en France
+                    </p>
+                </div>
+            </div>
+            <div class="flex flex-row">
+                <img class="max-h-40" src="public/Images/AnyConv.com__image3.webp" alt="">
+                <div class="w-[150px] flex-col pl-2 bg-[#E2E8F0]">
+                    <p class="font-lato text-2xl mb-3">2</p> <br/>
+                    <p class="-mt-10 font-lato font-semibold">Don’t Look Down</p> <br/>
+                    <p class=" font-montserrat text-black -mt-4 text-xs w-[145px]">
+                    - Plus de 250 millions d’écoutes sur les plateformes de streeming  <br/>
+                    - A obtenu le disque d’or dans plusieurs pays
+                    </p>
+                </div>
+            </div>
+            <div class="flex flex-row">
+                <img class="max-h-40" src="/public/Images/AnyConv.com__image4.webp" alt="">
+                <div class="w-[150px] flex-col pl-2 bg-[#E2E8F0]">
+                    <p class="font-lato text-2xl mb-3">3</p> <br/>
+                    <p class="-mt-10 font-lato font-semibold">In the name of love</p> <br/>
+                    <p class=" font-montserrat text-black -mt-4 text-xs w-[145px]">
+                    - Plus d’1 milliard de vues  <br/>
+                    - Cette chanson a obtenu le disque de diamant en France
+                    </p>
+                </div>
+            </div>
         </div>
 
 
